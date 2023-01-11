870009a2a38620825130e3183b825f55f40aa27a
James Smeda <james.smeda@gmail.com>
2023-01-11 10:50:21 +1030

    Add way to bookmark what you have read

---
+++ app/Http/Controllers/BibleBooksController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BibleBook;
+use Illuminate\Http\Request;
+
+class BibleBooksController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(BibleBook $bibleBook)
+    {
+    }
+
+    public function edit(BibleBook $bibleBook)
+    {
+    }
+
+    public function update(Request $request, BibleBook $bibleBook)
+    {
+    }
+
+    public function destroy(BibleBook $bibleBook)
+    {
+    }
+}
